843213bb7394e17a17dbfd812216342dbe12a933
Josh Lieberman <josh.lieberman92@gmail.com>
2014-03-28 17:56:51 -0400

    Initial commit of Flurry binding

---
+++ FlurryInterfaceDefs/IJavaObjectEx.cs
@@ -0,0 +1,11 @@
+﻿// Empty interface to eliminate these compile warnings
+// warning BG8C00: For type Java.Lang.Object, base interface Java.Interop.IJavaObjectEx does not exist.
+// warning BG8C00: For type Java.Lang.Throwable, base interface Java.Interop.IJavaObjectEx does not exist.
+
+namespace Java.Interop
+{
+	public interface IJavaObjectEx
+	{
+	}
+}
+
